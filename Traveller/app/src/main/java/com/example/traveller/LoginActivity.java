376c0705a6ef136f16b66173aa52138904879e82
AnaKenway <ancemilenkovic@gmail.com>
2021-06-18 17:23:21 +0200

    Kamera gotova
    Slikanje iz aplikacije i cuvanje slike na Firebase Cloud Storage-u

--- Traveller/app/src/main/java/com/example/traveller/LoginActivity.java
+++ Traveller/app/src/main/java/com/example/traveller/LoginActivity.java
@@ -3,10 +3,16 @@ package com.example.traveller;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.text.Layout;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +23,7 @@ import android.widget.Toast;
 import com.example.traveller.models.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -28,7 +35,16 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -39,6 +55,14 @@ public class LoginActivity extends AppCompatActivity {
     DatabaseReference myRef = database.getReference();
     private static final String FIREBASE_CHILD ="users";
     private String imgURI=null;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    String currentPhotoPath;
+    String storageImgPath;
+    //pogledaj ovde ako ne radi
+    FirebaseStorage storage=FirebaseStorage.getInstance();
+
+    // Create a Cloud Storage reference from the app
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,8 +90,7 @@ public class LoginActivity extends AppCompatActivity {
                     final EditText passwordEditText = findViewById(R.id.editTextLoginPassword);
                     String userName=usernameEditText.getText().toString();
                     String password=passwordEditText.getText().toString();
-                    //logika
-                    //myRef.child("traveller-user").
+
                     myRef.child("users").child(userName).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
                         @Override
                         public void onComplete(@NonNull Task<DataSnapshot> task) {
@@ -100,6 +123,8 @@ public class LoginActivity extends AppCompatActivity {
                     //preuzeti uri
                     //i sacuvati u imgURI
                     Toast.makeText(getApplicationContext(),"Kliknuta kamera",Toast. LENGTH_SHORT).show();
+                    dispatchTakePictureIntent();
+
                 }
             });
 
@@ -117,6 +142,7 @@ public class LoginActivity extends AppCompatActivity {
                     String username=etUsername.getText().toString();
                     String password=etPassword.getText().toString();
                     String email=etEmail.getText().toString();
+
 
                     User user=new User(etPassword.getText().toString(),
                             etEmail.getText().toString(),etFirstName.getText().toString(),etLastName.getText().toString(),
@@ -195,6 +221,104 @@ public class LoginActivity extends AppCompatActivity {
                         Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                Log.e("MYAPP", "exception", ex);
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                try {
+                    Uri photoURI = FileProvider.getUriForFile(this,
+                            "com.example.android.fileprovider",
+                            photoFile);
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                } catch(Exception e){
+                    Log.e("MYAPP", "exception", e);
+                }
+
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        storageImgPath=imageFileName;
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            //kod koji treba da se izvrsi kad se slika
+            galleryAddPic();
+            try {
+                InputStream stream = new FileInputStream(new File(currentPhotoPath));
+                StorageReference storageRef = storage.getReference("user_profile_pictures/"+storageImgPath);
+                UploadTask uploadTask;
+                uploadTask = storageRef.putStream(stream);
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        Toast.makeText(LoginActivity.this, "Couldn't upload image to cloud",
+                                Toast.LENGTH_LONG).show();
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                        // ...
+                        /*storageRef.getDownloadUrl().addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(LoginActivity.this, "Couldn't get image URL from Firebase Cloud",
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        }).addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                imgURI=uri.toString();
+                            }
+                        });*/
+                        //ukoliko nam treba nesto drugo kao img URI, vratiti se i ppogledati
+                        //================OVDE===================   
+                        imgURI=taskSnapshot.getMetadata().getPath();
+                    }
+                });
+            }catch(Exception ex){
+                Log.e("MYAPP", "exception", ex);
+            }
+
+        }
     }
 
 }
